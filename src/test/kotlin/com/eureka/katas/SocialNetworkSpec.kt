77fd4eb8be61d6538e246b11e76b7a97e77cf185
Angelo Sciarra <asciarra@byte-code.com>
2017-11-11 20:59:19 +0100

    initial commit

---
+++ src/test/kotlin/com/eureka/katas/SocialNetworkSpec.kt
@@ -0,0 +1,18 @@
+package com.eureka.katas
+
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.specs.StringSpec
+
+/**
+ * @author asciarra
+ */
+
+class SocialNetworkSpec: StringSpec() {
+    init {
+
+        "should fail" {
+            "a" shouldBe "b"
+        }
+
+    }
+}
